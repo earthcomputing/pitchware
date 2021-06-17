bd2704e920831b3b5bc1a6d8357dd7b45beb1be4
Steven Ganz <steven.ganz@teamifier.com>
2021-06-16 17:59:58 -0700

    Add license file and put license/copyright banner on source files.  In the process, ensure that all files end with newline.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright © 2016-present Earth Computing Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright © 2016-present Earth Computing Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 var express = require('express');
 var router = express.Router();
 
